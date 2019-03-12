c028c77ed9cb986ce01c633c913cdbebd32828f2
Yonas Fesehatsion <yone920@gmail.com>
2019-03-12 23:17:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
+var kittens = [
+  'Milo', 
+  'Otis', 
+  'Garfield'
+  ] //define your array here
 
 // Add your functions and code here
+  function destructivelyAppendKitten() {
+    kittens.push('Ralph')
+    return kittens
+  } 
+
+function destructivelyPrependKitten() {
+    kittens.unshift('Bob')
+    return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  let newAray = []
+  kittens.pop(name)
+  newAray.push()
+  return newAray
+}
+
